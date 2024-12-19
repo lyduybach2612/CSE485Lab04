550a9e6591f145eeefd7e3e1ed7299d82cf5590c
vuongchienn <vuongtatchien21122004@gmail.com>
2024-12-19 14:30:33 +0700

    completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Models\Borrow;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books',BookController::class);
